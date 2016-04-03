8c12ad9876497ef971068d1e742e069422a7a002
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-04-03 16:42:00 -0600

    move code into respective namespace

--- src/abq_transit/core.clj
+++ src/abq_transit/core.clj
@@ -1,60 +1,7 @@
 (ns abq-transit.core
-  (:gen-class)
-  (:require [clojure.xml :as xml]
-            [clojure.zip :as zip]
-            [clojure.string :as str]
-            [clojure.data.zip.xml :refer [xml1-> text]])
-  (:import (java.io InputStream)))
+  (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
-
-(defn parse-int [^String s]
-  (Integer/parseInt (re-find #"\A-?\d+" s)))
-
-(defn route [loc]
-  (-> loc
-      (xml1-> :Name text)
-      (str/split #"\s")
-      last
-      parse-int))
-
-(defn heading [loc]
-  (-> loc
-      (xml1-> :Placemark :Style :IconStyle :heading text)
-      parse-int))
-
-(defn coordinates [loc]
-  (-> loc
-      (xml1-> :Placemark :Point :coordinates text)
-      (str/split #",")))
-
-(defn lon [loc]
-  (-> loc
-      coordinates
-      first
-      read-string))
-
-
-(defn lat [loc]
-  (-> loc
-      coordinates
-      second
-      read-string))
-
-
-(defn parse-transit-kml
-  "Given an input stream to an ABQ Transit KML, a map containing keys for
-   route, heading, lon, and lat is returned. This function doesn't close
-   the input stream."
-  [^InputStream is]
-  (let [transit-zip (-> is
-                        xml/parse
-                        zip/xml-zip)
-        document (xml1-> transit-zip :Document)]
-    {:route   (route document)
-     :heading (heading document)
-     :lon     (lon document)
-     :lat     (lat document)}))
