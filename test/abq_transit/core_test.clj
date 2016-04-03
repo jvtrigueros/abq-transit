abf554a31adf67cd5c6c0374c3313c94479d5925
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-04-03 16:17:54 -0600

    fix misuse of threading macro, use xml1-> instead

--- test/abq_transit/core_test.clj
+++ test/abq_transit/core_test.clj
@@ -3,6 +3,7 @@
   (:require [clojure.test :refer :all]
             [clojure.xml :as xml]
             [clojure.zip :as zip]
+            [clojure.data.zip.xml :as zx]
             [clojure.java.io :as io]
             [abq-transit.core :refer :all]))
 
@@ -15,12 +16,11 @@
                       [xml (-> "route2.kml"
                                io/resource
                                io/input-stream)]
-                      (zip/xml-zip (xml/parse xml)))))
+                      (zip/xml-zip (xml/parse xml))))
+  (test-fn))
 
 (use-fixtures :once load-kml-route)
 
 (deftest route-number
   (testing "Verify that route number is correct."
-    (is (= 2 (-> @kml-route
-                 :Document
-                 route)))))
+    (is (= 2 (route (zx/xml1-> @kml-route :Document))))))
