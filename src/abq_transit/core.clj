c573e2e362b23acb41feee5b771c6ef8b77ad47a
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-04-03 22:22:39 -0600

    begin handling db connection

--- src/abq_transit/core.clj
+++ src/abq_transit/core.clj
@@ -2,13 +2,24 @@
   (:gen-class)
   (:use [clojure.pprint])
   (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [korma.db :as db]
             [abq-transit.parse :as transit-parse]))
+
+
+(defn- read-config [config-path]
+  (edn/read-string (slurp config-path)))
 
 (defn -main
   "Given a list of URLs, parse KML."
   [& args]
-  (doseq [route-url args]
-    (with-open [route (-> route-url
-                          io/input-stream)]
-      (pprint (transit-parse/parse-transit-kml route)))))
-
+  (if-let [config-path (first args)]
+    (let [config (read-config config-path)
+          db (-> (:db config)
+                 db/create-db
+                 db/default-connection)]
+      (doseq [route-url (:routes config)]
+          (with-open [route (-> route-url
+                                io/input-stream)]
+            (pprint (transit-parse/parse-transit-kml route)))))
+    (println "Please pass in an EDN configuration file as your first command line argument.")))
