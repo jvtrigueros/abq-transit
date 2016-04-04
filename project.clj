937d14da79cff0dcb1eeec08c4fc2e6235825b12
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-04-03 22:21:47 -0600

    add korma ORM and sqlite driver

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Apache License, Version 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.zip "0.1.1"]]
+                 [org.clojure/data.zip "0.1.1"]
+                 [korma "0.4.0"]
+                 [org.xerial/sqlite-jdbc "3.7.15-M1"]]
   :main ^:skip-aot abq-transit.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
